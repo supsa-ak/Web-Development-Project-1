4a5fb7083e2a83f9890738abdc1453d1890f1ea0
Sarthak <sarthakkalpande113@gmail.com>
2022-12-23 04:31:23 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("Welcome");
+
+document.getElementById('submit').addEventListener('click', 
+    function(event){event.preventDefault(myFunction())});
+
+function myFunction(){
+    document.getElementById('message').style.display = 'block';
+}
